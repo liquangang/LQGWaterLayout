32c5185c5c6f6adac71900b2f4ef26a841f98269
liquangang <liquangang@idstaff.com>
2017-04-18 17:39:40 +0800

    增加测试类

---
+++ LQGWaterLayout/LQGWaterLayoutTestObj.m
@@ -0,0 +1,13 @@
+//
+//  LQGWaterLayoutTestObj.m
+//  LQGWaterLayout
+//
+//  Created by quangang on 2017/4/18.
+//  Copyright © 2017年 LQG. All rights reserved.
+//
+
+#import "LQGWaterLayoutTestObj.h"
+
+@implementation LQGWaterLayoutTestObj
+
+@end
